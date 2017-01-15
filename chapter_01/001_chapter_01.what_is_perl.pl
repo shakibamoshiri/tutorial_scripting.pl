c14b769096c853added44956406a4154ca22bac4
Shekib <vhsu30@yahoo.com>
2017-01-15 19:38:47 +0330

    introduction

---
+++ chapter_01/001_chapter_01.what_is_perl.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+###########################
+# chapter:	1. Introduction
+###########################
+
+# There is some explanation about:
+# what is Perl
+#
+# See the main website:
+# www.tutorialspoint.com
+
