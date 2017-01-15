cba8123238d714c53723bca8ec3bbda90815a99e
Shekib <vhsu30@yahoo.com>
2017-01-15 19:39:09 +0330

    Environment

---
+++ chapter_02/002_chapter_02.install_perl.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+############################
+# chapter:	2. Environment
+############################
+
+=comment
+What version of Perl do you have?
+	perl -v
+
+How can I install it?
+	Open a web browser and go to http://www.perl.org/get.html
+	then download the last version
+	
+	Extracts the file:
+	tar -xzf perl...
+	cd perl...
+	./configure -de
+	make
+	make test
+	make install
+
+How about window?
+	Search in the Internet and you find
+	something like:
+	http://strawberryperl.com
+	
+	then you can download the binary file and install it.
+=cut 
