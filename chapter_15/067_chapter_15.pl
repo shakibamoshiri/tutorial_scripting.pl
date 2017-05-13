fc6158a6b9af32c80909e4ed542eebe39f965d82
Shekib <vhsu30@yahoo.com>
2017-05-13 13:33:30 +0430

    formats

---
+++ chapter_15/067_chapter_15.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+###########################
+# chapter:      15. formats
+###########################
+
+=comment
+
+
+=cut
+
+
+
+# main function
+main( @ARGV );
+sub main {
+ 
+
+	# this chapter was ignored
+
+return 0;
+}
+
+
+
+
+
+
+=output for me
+
+
+
+=cut
