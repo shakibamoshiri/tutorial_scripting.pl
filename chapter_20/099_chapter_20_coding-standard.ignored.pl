bc3a6dc079822f60f4f986adfa4d91183f2db8a8
Shekib <vhsu30@yahoo.com>
2017-05-13 13:35:04 +0430

    coding in standard way

---
+++ chapter_20/099_chapter_20_coding-standard.ignored.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+#use strict;
+#use warnings;
+####################################
+# chapter:      20. coding standard
+####################################
+
+=comment
+ 
+	You should code for learning how to write standard code
+	in theory it looks easy, but in act of course not, so 
+	practice ...
+
+=cut
+
+
+
+
+=output for me
+
+
+=cut
