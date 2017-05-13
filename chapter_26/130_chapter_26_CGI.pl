ba1692d5829297b89d62c4a9d6b4bc1a6a3d0119
Shekib <vhsu30@yahoo.com>
2017-05-13 13:36:40 +0430

    CGI

---
+++ chapter_26/130_chapter_26_CGI.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+###################################
+# chapter:      26. Common Gateway Interface (CGI)
+###################################
+
+=comment
+
+	this chapter was ignored, 	
+=cut
+
+
+print "Content-type:text/html/r/n/r/n";
+print '<html>';
+print '<head>';
+print '<title> hello </title>';
+print '</head>';
+print '<body>';
+print '<h2> hello Perl </h2>';
+print '</body>';
+print '</html>';
+
+1;
+
+
+=output for me
+
+
+=cut
+print "\n" x 2;
