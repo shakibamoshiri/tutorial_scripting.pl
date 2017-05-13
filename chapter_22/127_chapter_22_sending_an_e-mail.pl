443659c93e86a3391a00d9845bdb41594a228232
Shekib <vhsu30@yahoo.com>
2017-05-13 13:35:41 +0430

    sending e-mail

---
+++ chapter_22/127_chapter_22_sending_an_e-mail.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+##################################
+# chapter:      22. sending e-mail
+##################################
+
+=comment
+
+	this chapter was ignored, 	
+=cut
+
+
+
+
+
+=output for me
+
+
+=cut
+print "\n" x 2;
