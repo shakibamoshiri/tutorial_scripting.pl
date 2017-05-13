a2292c5d6ef867a36c7237191e8ea5cadbfb8154
Shekib <vhsu30@yahoo.com>
2017-05-13 13:37:47 +0430

    embeded Doc

---
+++ chapter_29/143_29_embedded_documentation.ignored.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+
+##########################################
+# chapter:      29. embedded documentation
+##########################################
+
+=comment
+	this chapter was ignored
+=cut
+
+
+
+=output for me
+
+
+
+=cut
