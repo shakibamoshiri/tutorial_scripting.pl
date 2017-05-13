57321227f3bc05006e57422454e6c726f401b972
Shekib <vhsu30@yahoo.com>
2017-05-13 13:36:30 +0430

    database accessing

---
+++ chapter_25/129_chapter_25_database_access.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use v5.22.1;
+
+use strict;
+use warnings;
+###################################
+# chapter:      25. database access
+###################################
+
+=comment
+
+	this chapter was ignored, 	
+=cut
+
+
+
+
+
+=output for me
+
+
+=cut
+print "\n" x 2;
